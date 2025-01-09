41bbacdf05b85bf2c10aced918d56b3fd6ddc1ba
unknown <chrisbennie0036@gmail.com>
2025-01-09 16:23:04 +0100

    Tidied up the shared projects and added Dockerfile and .dockerignore

--- RecipeRandomizer/RecipeRandomizer.Shared.Constants/RateLimiterConstants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipeRandomizer.Shared.Constants;
-
-public static class RateLimiterConstants
-{
-    public const string PostRateLimiterPolicyName = "PostRateLimiterPolicy";
-    public const string AnonymousUserRateLimiterPolicyName = "AnonymousUserRateLimiterPolicy";
-
-}
